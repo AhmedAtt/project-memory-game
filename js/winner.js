ef5083c29a80a785cb198c2f019bba8897ddcede
AhmedAtt <ahmedfattia@outlook.com>
2018-01-31 11:28:41 +0200

    Add files via upload

---
+++ js/winner.js
@@ -0,0 +1,7 @@
+var userRating =sessionStorage.getItem('rating');
+var userTime=sessionStorage.getItem('time');
+for(var m=0; m<userRating; m++){
+    $("#star-rating").append("<li><i class='fa fa-star'></i></li>");
+      console.log("YOU GOT STAR");
+}
+$('#user-time').text(userTime);
